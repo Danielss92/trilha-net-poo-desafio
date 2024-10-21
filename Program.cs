c6839bdc7c3c5bc3b27e632985b954a5953df714
Daniel da Silva <danielsilvasouza92@gmail.com>
2024-10-21 13:54:29 -0300

    Desafio Abstracao Sistema celular POO concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero:"123456",modelo: "Modelo 1",imei: "22121212",memoria: 52);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero:"654321",modelo: "Modelo 2",imei: "44441212",memoria: 60);
+iphone.Ligar();
+iphone.InstalarAplicativo("Facebook");
